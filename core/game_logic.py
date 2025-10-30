bb5b4aa9f177f7c21a9f3e7d82432701c1609d5f
Troppmop <troppmop@gmail.com>
2025-10-30 09:36:40 +0200

    skeleton code

---
+++ core/game_logic.py
@@ -0,0 +1,11 @@
+def calculate_hand_value(hand: list[dict]) -> int:
+    return 0 
+
+def deal_two_each(deck:list[dict], player: dict, dealer: dict)-> None:
+    pass
+
+def dealer_play(deck:list[dict], dealer:dict)->bool:
+    return False
+
+def run_full_game(desk:list[dict], player:dict, dealer:dict)-> None:
+    pass
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from core import game_logic, deck, player_io
 
+if __name__ == "__main__":
+    game_deck = deck.build_standard_deck()
+    shuffled_deck = deck.shuffle_by_suit(game_deck)
+
+    player = {'hand':[]}
+    dealer = {'hand':[]}
+
+    game_logic.run_full_game(game_deck,player, dealer)
